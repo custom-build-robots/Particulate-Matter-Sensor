be84c07f57a43648f1c0eda7508e206912593676
Ingmar Stapel <github@stapel-ingmar.de>
2017-05-21 18:21:48 +0200

    Update sds_start.sh

--- sds_start.sh
+++ sds_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-/bin/sleep 10
-gpsd -b /dev/ttyACM0 -F /var/run/gpsd.sock -G >> /home/pi/feinstaub/gpsd_daemon.log 2>&1 &
+# /bin/sleep 10
+# Now I am using the gpsd auto start config.
+# gpsd -b /dev/ttyACM0 -F /var/run/gpsd.sock -G >> /home/pi/feinstaub/gpsd_daemon.log 2>&1 &
 /bin/sleep 10
 python /home/pi/feinstaub/sds011_2.py >> /home/pi/feinstaub/python_sds011.log 2>&1 &
